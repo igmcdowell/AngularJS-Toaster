9590920c71b63aaaf1d1727fa95a23abe5c4eca5
igmcdowell <ian@handl.it>
2014-09-16 15:13:45 -0700

    Fix missing concat

--- toaster.js
+++ toaster.js
@@ -172,7 +172,7 @@ function ($compile, $timeout, $sce, toasterConfig, toaster) {
         template:
         '<div  id="toast-container" ng-class="config.position">' +
             '<div ng-repeat="toaster in toasters" class="toast" ng-class="toaster.type" ng-click="click(toaster)" ng-mouseover="stopTimer(toaster)"  ng-mouseout="restartTimer(toaster)">' +
-              '<button class="toast-close-button" ng-show="config.closeButton" ng-click="removeToast(toaster.id)">&times;</button>'
+              '<button class="toast-close-button" ng-show="config.closeButton" ng-click="removeToast(toaster.id)">&times;</button>' +
               '<div ng-class="config.title">{{toaster.title}}</div>' +
               '<div ng-class="config.message" ng-switch on="toaster.bodyOutputType">' +
                 '<div ng-switch-when="trustedHtml" ng-bind-html="toaster.html"></div>' +
